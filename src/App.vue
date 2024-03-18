e2d789349261c66bf8d0e1fcb4599e3cb4971e84
赵江江 <zhaojiangjiang@kingyee.com.cn>
2024-03-18 14:51:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button as ButtonFive } from 'vant-v485';
+import { Button as ButtonSix } from 'vant-v486';
+</script>
+
+<template>
+  <button-five></button-five>
+  <button-six></button-six>
+</template>
+
+<style scoped></style>
